c6902743fa21e93e9f04b64c708c19cb6111a8bc
Deepak N <endeep123@gmail.com>
2011-06-19 21:34:40 +0530

    Added testcase decorator and named method decorator to test

--- pungi/dec.py
+++ pungi/dec.py
@@ -1,9 +1,19 @@
 import pungi
 
 
-def spied(function):
+def test(function):
 
     def decorator(self):
         function(self)
         pungi.stopSpying()
     return decorator
+
+def testcase(cls):
+    test_methods = filter(lambda name: name.startswith("test_"), cls.__dict__.keys())
+    for method_name in test_methods:
+        method = getattr(cls, method_name)
+        def decorator(self):
+            method(self)
+            pungi.stopSpying()
+        setattr(cls, method_name, decorator)
+    return cls
--- tests/test_dec.py
+++ tests/test_dec.py
@@ -9,12 +9,28 @@ class TempClass(object):
         return "hello"
 
 
-class SpiedMethodDecoratorTest(unittest.TestCase):
+class MethodDecoratorTest(unittest.TestCase):
 
     def setUp(self):
         self.obj = TempClass()
 
-    @pungi.dec.spied
+    @pungi.dec.test
+    def test_spied_decorator_stops_spying_after_running_the_test(self):
+        pungi.spyOn(self.obj, 'hello', returnValue="spy says hello")
+
+        self.assertEqual(self.obj.hello(), "spy says hello")
+
+    def tearDown(self):
+        self.assertEqual(self.obj.hello(), "hello",
+                        "Decorator did not stop spying!!")
+
+
+@pungi.dec.testcase
+class ClassDecoratorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.obj = TempClass()
+
     def test_spied_decorator_stops_spying_after_running_the_test(self):
         pungi.spyOn(self.obj, 'hello', returnValue="spy says hello")
 
