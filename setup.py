049f63118fea861b65f7f20e6ce432242ee660ca
Deepak N <endeep123@gmail.com>
2011-07-03 17:49:45 +0530

    Added setup script

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(name='Pungi',
+      version='0.1.0',
+      description='Simple and powerful mocking framework with '
+                  'extensible assertion matchers',
+      author='Deepak N',
+      author_email='endeep123@gmail.com',
+      maintainer='Deepak N',
+      maintainer_email='endeep123@gmail.com',
+      url='https://github.com/endeepak/pungi',
+      packages=['pungi'],
+      license="MIT"
+     )
